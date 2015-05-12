cd3c9a88b028f15260fb289f231556fb862d7ab5
Stevenlin0510 <stevenlin0510@gmail.com>
2015-05-12 16:43:03 +0800

    网易云音乐MV，曲名，歌手信息捕捉更新

--- 163yunyinyue.py
+++ 163yunyinyue.py
@@ -15,30 +15,16 @@ def getVideo(html):
 	imglist = re.findall(imgre, html)
 	return imglist
 
-# for num in range(393004, 393007):
-# 	print num
-# 	html = getHTML('http://music.163.com/mv?id=%s'%num)
-# 	parsed = getVideo(html)
-# 	print parsed
-
 ID = raw_input("Input the MV ID: ")
-
 url = 'http://music.163.com/mv?id=' + str(ID)
 
 html = getHTML(url)
 parsed = getVideo(html)
-# print parsed
 videoUrls = parsed[0].split("&")
-# print videoUrls
-
 mp4Url = videoUrls[0].strip()
-print "mp4Url: " + mp4Url
-
 trackName = videoUrls[3].split("=")[1].strip()
-print "Song: " + trackName
-
 artistName = videoUrls[4].split("=")[1].strip()
-print "Artist: " + artistName
 
+print "mp4Url: " + mp4Url + '\n' + "Song: " + trackName + '\n' + "Artist: " + artistName
+ 
 # filename = '%s/%s.mp4' % (artistName, trackName)
-# print filename
